905120a25b8325680f48250005a344dd74096980
Edgar779 <41956733+Edgar779@users.noreply.github.com>
2020-05-19 22:59:22 +0400

    Update user.js
    some-change

--- server/controller/user.js
+++ server/controller/user.js
@@ -9,92 +9,8 @@ const Op = db.Sequelize.Op;
 const passport = require('passport');
 
 var LocalStrategy = require('passport-local').Strategy;
-// router.use(passport.initialize());
-// router.use(passport.session());
-
-
-
-// passport.serializeUser(function (profile, cb) {
-//     cb(null, profile);
-// });
-
-// passport.deserializeUser(function (obj, cb) {
-//     cb(null, obj);
-// });
-
-
-
-// passport.serializeUser(function(user, done) {
-//     done(null, user.id);
-//   });
-  
-  
-//   passport.deserializeUser(function(id, done) {
-//     User.findById(id, function(err,user){
-//       err 
-//         ? done(err)
-//         : done(null,user);
-//     });
-//   });
-
-
-
-
-
-//   passport.use(new LocalStrategy({
-//     usernameField: 'email',
-//     passwordField: 'password'
-//   }, function(username, password,done){
-//     User.findOne({ username : username},function(err,user){
-//       return err 
-//         ? done(err)
-//         : user
-//           ? password === user.password
-//             ? done(null, user)
-//             : done(null, false, { message: 'Incorrect password.' })
-//           : done(null, false, { message: 'Incorrect username.' });
-//     });
-//   }));
-
 
 exports.signup = (req, res) => {
-  // User.destroy({
-  //   where: {},
-  //   truncate: true
-  // }).then(ress=>{
-  //   console.log(ress);
-  // }).catch(err=>{
-  //   console.log(err);
-  // })
-
-
-//    User.findAll({ where: { fullName: req.body.fullName } })
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || "Some error occurred while retrieving tutorials."
-//       });
-//     });
-
-
-
-
-
-
-
-
-
-    // if (!req.body.fullName || !req.body.email || !req.body.password) {
-    //   res.status(200).send({
-    //     success: false,
-    //     message: "Content can not be empty!"
-    //   });
-    //   return;
-    // }
-
    
     const user = {
         fullName: req.body.fullName,
@@ -117,19 +33,6 @@ exports.signup = (req, res) => {
           });
         });
     });
-
-
-    //    User.findAll({ where: { } })
-    // .then(data => {
-    //   res.send(data);
-    // })
-    // .catch(err => {
-    //   res.status(500).send({
-    //     message:
-    //       err.message || "Some error occurred while retrieving tutorials."
-    //   });
-    // });
-
     
   };
 
@@ -137,20 +40,6 @@ exports.signup = (req, res) => {
    res.send('login success');
     
   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 exports.register = function (req, res) {
@@ -176,27 +65,3 @@ exports.register = function (req, res) {
     });
 
 }
-// get User By Token
-// exports.getUserbyToken = function (req, res) {
-//     const token = req.header('x-auth');
-//     var decoded;
-//     try {
-//         decoded = jwt.verify(token, config.jwt_secretKey);
-//     }
-//     catch (e) {
-//         return res.status(401).send(e);
-//     }
-//     users.findOne({
-//         '_id': decoded._id,
-//         'tokens.token': token,
-//         'tokens.access': 'auth'
-//     }).then(user => {
-//         if (!user) {
-//             res.json('user is not defined');
-//         }
-//         res.json(user);
-//     }).catch(err => {
-//         res.json(err);
-//     })
-
-// }
